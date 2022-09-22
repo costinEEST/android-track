16c42fb8ae62e7db14b259468b54ab498c8f52c7
Constantin Câmpean <mail.la.costi@gmail.com>
2022-09-22 20:46:14 +0300

    add a debug log statement

--- android-developer-fundamentals/app/src/main/java/com/example/first_app/MainActivity.java
+++ android-developer-fundamentals/app/src/main/java/com/example/first_app/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.first_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
     }
 }
